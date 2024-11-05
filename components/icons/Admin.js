0a30fc4e7330805d1ba759d190a3ec40e411670d
Hanan0631 <hm200390@gmail.com>
2024-11-05 20:51:50 +0330

    Components of icons and necessary images added to the project

---
+++ components/icons/Admin.js
@@ -0,0 +1,37 @@
+function Admin() {
+  return (
+    <svg
+      fill="#282828"
+      height="40px"
+      width="40px"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="-2.4 -2.4 28.80 28.80"
+      enableBackground="new 0 0 24 24"
+      xmlSpace="preserve"
+      stroke="#282828"
+      transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"
+      strokeWidth="0.00024000000000000003"
+    >
+      <g id="SVGRepoBgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepoTracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke="#CCCCCC"
+        strokeWidth="0.192"
+      ></g>
+      <g id="SVGRepoIconCarrier">
+        {" "}
+        <g id="userAdmin">
+          {" "}
+          <path d="M22.3,16.7l1.4-1.4L20,11.6l-5.8,5.8c-0.5-0.3-1.1-0.4-1.7-0.4C10.6,17,9,18.6,9,20.5s1.6,3.5,3.5,3.5s3.5-1.6,3.5-3.5 c0-0.6-0.2-1.2-0.4-1.7l1.9-1.9l2.3,2.3l1.4-1.4l-2.3-2.3l1.1-1.1L22.3,16.7z M12.5,22c-0.8,0-1.5-0.7-1.5-1.5s0.7-1.5,1.5-1.5 s1.5,0.7,1.5,1.5S13.3,22,12.5,22z"></path>{" "}
+          <path d="M2,19c0-3.9,3.1-7,7-7c2,0,3.9,0.9,5.3,2.4l1.5-1.3c-0.9-1-1.9-1.8-3.1-2.3C14.1,9.7,15,7.9,15,6c0-3.3-2.7-6-6-6 S3,2.7,3,6c0,1.9,0.9,3.7,2.4,4.8C2.2,12.2,0,15.3,0,19v5h8v-2H2V19z M5,6c0-2.2,1.8-4,4-4s4,1.8,4,4s-1.8,4-4,4S5,8.2,5,6z"></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
+}
+
+export default Admin;
