22ce92f5b20bd05d971be06e31f1419d7e31e578
Hanan0631 <hm200390@gmail.com>
2024-11-09 01:03:46 +0330

    Some authorization added

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,16 @@
+//cookies-next
+import { getCookie } from "cookies-next";
+
+//next
+import { useRouter } from "next/router";
+
+//components
 import Login from "@/components/templates/Login";
 
 function login() {
+  const router = useRouter();
+  const cookie = getCookie("token");
+  if (cookie) router.push("/admin");
   return (
     <div>
       <Login />
--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,21 @@
-import Register from "@/components/templates/Register"
+//cookies-next
+import { getCookie } from "cookies-next";
+
+//next
+import { useRouter } from "next/router";
+
+//components
+import Register from "@/components/templates/Register";
 
 function register() {
+  const router = useRouter();
+  const cookie = getCookie("token");
+  if (cookie) router.push("/admin");
   return (
     <div>
       <Register />
     </div>
-  )
+  );
 }
 
-export default register
+export default register;
